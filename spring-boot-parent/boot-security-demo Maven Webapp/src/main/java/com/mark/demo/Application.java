bec7a6ee52c619add842d7670fef1b44a04d1294
hxp <hxpwangyi@163.com>
2017-09-24 19:43:05 +0800

    111

--- spring-boot-parent/boot-security-demo Maven Webapp/src/main/java/com/mark/demo/Application.java
+++ spring-boot-parent/boot-security-demo Maven Webapp/src/main/java/com/mark/demo/Application.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 /*
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan
+@ServletComponentScan
 @MapperScan("com.mark.demo.security.mapper")
 public class Application {
 
