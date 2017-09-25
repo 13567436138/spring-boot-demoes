59c260d3c9b172f7a7912ab63e2ad189df0659cc
hxp <hxpwangyi@163.com>
2017-09-25 12:46:30 +0800

    111

---
+++ spring-boot-parent/boot-dubbo-parent Maven Webapp/boot-dubbo-api/src/main/java/com/mark/demo/boot_dubbo_api/App.java
@@ -0,0 +1,13 @@
+package com.mark.demo.boot_dubbo_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
