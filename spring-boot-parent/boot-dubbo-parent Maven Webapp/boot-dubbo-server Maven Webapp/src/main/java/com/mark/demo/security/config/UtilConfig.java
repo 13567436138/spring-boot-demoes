4a9fbdbc8083324c035e3f844f440f6e27f99723
hxp <hxpwangyi@163.com>
2017-09-25 14:14:13 +0800

    111

---
+++ spring-boot-parent/boot-dubbo-parent Maven Webapp/boot-dubbo-server Maven Webapp/src/main/java/com/mark/demo/security/config/UtilConfig.java
@@ -0,0 +1,33 @@
+package com.mark.demo.security.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.mark.demo.security.utils.JedisUtils;
+import com.mark.demo.security.utils.SpringUtils;
+
+/*
+*hxp(hxpwangyi@126.com)
+*2017年9月16日
+*
+*/
+@Configuration
+public class UtilConfig {
+
+	
+	@Bean(name="springUtils")
+	public SpringUtils springUtils(ApplicationContext applicationContext){
+		SpringUtils springUtils=new SpringUtils();
+		springUtils.setApplicationContext(applicationContext);
+		return springUtils;
+	}
+	
+	@Bean
+	public JedisUtils jedisUtil(RedisTemplate<String, Object> redisTemplate){
+		JedisUtils jedisUtil=new JedisUtils();
+		jedisUtil.setRedisTemplate(redisTemplate);
+		return jedisUtil;
+	}
+}
