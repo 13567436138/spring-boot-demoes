2bf412d2c91246275bd1fcf061818599f4e7e117
hxp <hxpwangyi@163.com>
2017-10-08 20:12:38 +0800

    111

--- spring-boot-parent/boot-gemfire-demo Maven Webapp/src/main/java/com/mark/demo/Application.java
+++ spring-boot-parent/boot-gemfire-demo Maven Webapp/src/main/java/com/mark/demo/Application.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.gemfire.config.annotation.CacheServerApplication;
+import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
+import org.springframework.data.gemfire.config.annotation.EnablePdx;
+import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
+import org.springframework.data.gemfire.transaction.config.EnableGemfireCacheTransactions;
 
 /*
 *hxp(hxpwangyi@126.com)
@@ -15,6 +20,12 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan
 @ServletComponentScan
+@CacheServerApplication(locators = "localhost[1099]",logLevel = "info", autoStartup = true, maxConnections = 100)
+@EnablePdx(serializerBeanName = "compositePdxSerializer")
+@EnableEntityDefinedRegions(basePackages = "com.mark.demo.security.entity")
+@EnableGemfireRepositories(basePackages={"com.mark.demo.security.repository"})
+@EnableGemfireCacheTransactions
+
 public class Application {
 
 	public static void main(String[] args) {
--- spring-boot-parent/boot-gemfire-demo Maven Webapp/src/main/java/com/mark/demo/security/config/GemfireConfig.java
+++ spring-boot-parent/boot-gemfire-demo Maven Webapp/src/main/java/com/mark/demo/security/config/GemfireConfig.java
@@ -1,6 +1,14 @@
 package com.mark.demo.security.config;
 
+import javax.transaction.UserTransaction;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.gemfire.config.annotation.CacheServerConfigurer;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+import com.gemstone.gemfire.pdx.PdxSerializer;
+import com.gemstone.gemfire.pdx.ReflectionBasedAutoSerializer;
 
 /*
 *hxp(hxpwangyi@126.com)
@@ -9,5 +17,24 @@ import org.springframework.context.annotation.Configuration;
 */
 @Configuration
 public class GemfireConfig {
+	  @Bean
+	  CacheServerConfigurer cacheServerPortConfigurer(String cacheServerHost,int cacheServerPort) {
 
+	      return (beanName, cacheServerFactoryBean) -> {
+	          cacheServerFactoryBean.setBindAddress(cacheServerHost);
+	          cacheServerFactoryBean.setPort(cacheServerPort);
+	      };
+	  }
+	  
+	  @Bean
+	  PdxSerializer compositePdxSerializer() {
+	      return new ReflectionBasedAutoSerializer();
+	  }
+	  
+	  @Bean
+	  public JtaTransactionManager transactionManager(UserTransaction userTransaction) {
+	     JtaTransactionManager transactionManager = new JtaTransactionManager();
+	     transactionManager.setUserTransaction(userTransaction);
+	     return transactionManager;
+	  }
 }
