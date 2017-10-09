fe0cab4337ba6908a169eeefdaa9f9fa96d02add
hxp <hxpwangyi@163.com>
2017-10-09 16:43:26 +0800

    111

---
+++ spring-boot-parent/boot-jersey-demo Maven Webapp/src/main/java/com/mark/demo/security/config/MyJerseryConfig.java
@@ -0,0 +1,12 @@
+package com.mark.demo.security.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+/*
+*hxp(hxpwangyi@126.com)
+*2017年10月9日
+*
+*/
+public class MyJerseryConfig extends ResourceConfig{
+
+} 
