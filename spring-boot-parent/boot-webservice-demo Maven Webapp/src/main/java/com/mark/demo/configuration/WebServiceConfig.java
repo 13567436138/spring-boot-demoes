0f60bdd6dc1137dcfa84f4c099b371c58fb168a2
hxp <hxpwangyi@163.com>
2017-10-12 20:14:55 +0800

    111

--- spring-boot-parent/boot-webservice-demo Maven Webapp/src/main/java/com/mark/demo/configuration/WebServiceConfig.java
+++ spring-boot-parent/boot-webservice-demo Maven Webapp/src/main/java/com/mark/demo/configuration/WebServiceConfig.java
@@ -1,12 +1,11 @@
 package com.mark.demo.configuration;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
@@ -18,9 +17,17 @@ import org.springframework.xml.xsd.XsdSchema;
 *2017年10月12日
 *
 */
+@EnableWs
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
 	
+	/*@Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }*/
 	
 	@Bean(name = "countries")
     public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
